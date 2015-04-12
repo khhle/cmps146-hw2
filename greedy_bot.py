3a5cc061a64fed6813072c21e64002f295702d41
Patrick <pcrussel@ucsc.edu>
2015-04-11 17:44:03 -0700

    Fixed typos
    Code works now

--- greedy_bot.py
+++ greedy_bot.py
@@ -5,10 +5,10 @@ def think(state, quip):
     topScore = 0
     primeMove = random.choice(state.get_moves())
 
-    player = state.get_shows_turn()
+    player = state.get_whos_turn()
     score = state.get_score()[player]
     
-    for move in state.get_move():
+    for move in state.get_moves():
       tempState = state.copy()
       tempState.apply_move(move)
       if(tempState.get_score()[player] + score) > topScore:
